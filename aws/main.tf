67fec2599c6862ac542b9d4f54da3401c2635b27
aduobed <obedus10@gmail.com>
2025-05-23 16:57:37 -0500

    add code for terraform init changes

---
+++ aws/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+#Configure VPC and subnets
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name    = "development-vpc"
+    vpc_env = "test"
+  }
+}
+
+resource "aws_subnet" "development-subnet-1" {
+  vpc_id            = aws_vpc.development-vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "development-subnet-1"
+  }
+
+}
+data "aws_vpc" "existing-vpc" {
+  default = true
+}
+
+resource "aws_subnet" "development-subnet-2" {
+  vpc_id            = data.aws_vpc.existing-vpc.id
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "development-subnet-2"
+  }
+}
